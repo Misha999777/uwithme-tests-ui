61fc299bbc9b163ae550313c8298457bf1dd6840
Mykhailo Hroshevyi <misha999777@gmail.com>
2023-05-27 14:21:20 +0200

    CI/CD

--- public/env.template.js
+++ public/env.template.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-template-curly-in-string */
 window["env"] = window["env"] || {};
 
-window["env"]["BASE_URL"] = "https://${TESTSYSTEM_SERVER_HOST}";
-window["env"]["AUTHORITY"] = "https://${TESTSYSTEM_KEYCLOAK_HOST}/realms/${TESTSYSTEM_KEYCLOAK_REALM}";
+window["env"]["BASE_URL"] = "${SCHEMA}${TESTSYSTEM_SERVER_HOST}";
+window["env"]["AUTHORITY"] = "${SCHEMA}${TESTSYSTEM_KEYCLOAK_HOST}/realms/${TESTSYSTEM_KEYCLOAK_REALM}";
